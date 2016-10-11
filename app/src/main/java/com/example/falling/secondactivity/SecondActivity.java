ed6c4052f1db07f007e5ec15b4cadc4883cafad4
heqiuling <290735636@qq.com>
2016-10-11 10:34:03 +0800

    first commit

---
+++ app/src/main/java/com/example/falling/secondactivity/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.falling.secondactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by falling on 16/4/15.
+ */
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
